7c726e7810436a9169400222c6faf48200a2536e
Furkan Gul <frkangul@gmail.com>
2023-08-27 22:27:07 +0300

    fix: callback issue, remove None

--- src/utils.py
+++ src/utils.py
@@ -117,7 +117,7 @@ def setup_pl_callbacks(cfg):
         TQDMProgressBar(refresh_rate=20), # for notebook usage
         # earlystop_checkpointer,
         model_checkpointer,
-        LogSegPredictionCallback() if cfg.dataset.mode == "binary" else None,
+        LogSegPredictionCallback() if cfg.dataset.mode == "binary" else,
         lr_monitor,
         # ReduceLROnPlateauOptCallback()
     ]
